52d97724083fbbbb7318266fc8caf2383a38687e
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-03 15:41:01 -0400

    added working gif search

--- app.js
+++ app.js
@@ -20,8 +20,15 @@ app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-    console.log(req.query) 
-    res.render('home')
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            const gifs = response;
+            res.render('home', { gifs })
+        }).catch(console.error);
   })
 
 app.get('/greetings/:name', (req, res) => {
@@ -34,3 +41,9 @@ app.get('/greetings/:name', (req, res) => {
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
+
+const Tenor = require("tenorjs").client({
+    "Key": "AIzaSyDOQPgewIp7yCqW7DG5cCVM_WXg4Xw9Irg", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+  });
