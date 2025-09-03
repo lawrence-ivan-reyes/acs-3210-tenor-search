543ceab82750d19bdca9f1a4703790e02cddee34
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-03 15:11:14 -0400

    added search form

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const express = require('express');
 const app = express();
 
 // Middleware
-// Allow Express (our web framework) to render HTML templates and send them back to the client using a new function
 const handlebars = require('express-handlebars');
 
 const hbs = handlebars.create({
-    // Specify helpers which are only registered on this instance.
     helpers: {
         foo() { return 'FOO!'; },
         bar() { return 'BAR!'; }
@@ -22,17 +20,12 @@ app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+    console.log(req.query) 
+    res.render('home')
   })
 
-
 app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
     const name = req.params.name;
-    // render the greetings view, passing along the name
     res.render('greetings', { name });
   })
 
